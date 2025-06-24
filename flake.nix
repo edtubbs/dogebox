b712155f0b26ce6cc29bb90cb4ba7520667c7418
Ando <divinegod@gmail.com>
2025-06-24 10:25:01 +1000

    Fix setOpt script to check /opt/dbx-installed at run-time

--- flake.nix
+++ flake.nix
@@ -67,17 +67,17 @@
 
       getSetOptScript =
         builderType: isBaseBuilder:
-        let
-          isReadOnly = (builderType == "iso" || builderType == "nanopc-t6");
-          isInstalled = builtins.pathExists /opt/dbx-installed;
-          mediaFile = if (isReadOnly && !isInstalled) then "ro-media" else "rw-media";
-        in
         ''
           mkdir -p /opt
           echo '${builderType}' > /opt/build-type
-          rm -rf /opt/ro-media
-          rm -rf /opt/rw-media
-          touch /opt/${mediaFile}
+
+          if [ -f /opt/dbx-installed ]; then
+            rm -f /opt/ro-media
+            touch /opt/rw-media
+          else
+            rm -f /opt/rw-media
+            touch /opt/ro-media
+          fi
         '';
 
       versionScript =
