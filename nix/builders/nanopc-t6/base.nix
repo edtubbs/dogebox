e0cd88d104db40eaee091d4ab0734a45c5b1381c
Anders Thor Nando <divinegod@gmail.com>
2025-11-27 09:28:59 +1100

    feat(t6): Setup Wi-Fi AP
    Sets a Wi-Fi AP with SSID Dogebox, Passphrase SuchPass

--- nix/builders/nanopc-t6/base.nix
+++ nix/builders/nanopc-t6/base.nix
@@ -149,6 +149,24 @@
     };
   };
 
+  # Setup a Wi-Fi Access Point for initial configuration.
+  # This allows a user to configure the dogebox by only plugging in power
+  # Caveat: Upon configuring the OS with a proper Wi-Fi network, the user will
+  # have to reconnect to that network and manually reload the dpanel page.
+  networking.wireless.enable = true;
+
+  services.create_ap = {
+    enable = true;
+    settings = {
+      INTERNET_IFACE = "lo";
+      WIFI_IFACE = "wlP3p49s0";
+      SSID = "Dogebox";
+      PASSPHRASE = "SuchPass";
+    };
+  };
+
+  networking.wireless.interfaces = [ "wlP3p49s0" ];
+
   systemd.services.resizerootfs = {
     description = "Expands root filesystem of boot device on first boot";
     unitConfig = {
